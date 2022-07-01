ded74ccbbdbd5f84865970b69c038c9c7fb007ee
Paul Seal <prjseal@gmail.com>
2022-07-01 23:17:38 +0100

    added content page added blog section added block list preview

---
+++ Portfolio.Core/Composing/StartupComposer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Core.NotificationsHandlers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+
+namespace _24Days.Core.Composers
+{
+    internal class StartupComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder
+                .AddNotificationHandler<ServerVariablesParsingNotification,
+                    ServerVariablesParsingNotificationHandler>();
+        }
+    }
+}
